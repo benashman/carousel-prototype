b7b123ccdb2db1bbebdba12b9a9d04f261150e03
Ben Ashman <ben.ashman@me.com>
2015-10-31 12:48:46 -0700

    add settings view

--- carousel-prototype/SettingsViewController.swift
+++ carousel-prototype/SettingsViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scrollView.contentSize = imageView.frame.size
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +24,7 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didTapCloseButton(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
-    */
-
 }
